ba0ceff520671a8c9a7688d72af5bff7bed7d325
Timur Evgazhukov <evgajukov@gmail.com>
2020-11-08 10:49:40 +0300

    визуальное представление квартир, в которых есть жильцы и в которых нет

--- src/pages/home/FloorPage.vue
+++ src/pages/home/FloorPage.vue
@@ -3,14 +3,14 @@
     <v-row dense>
       <v-col v-for="flat in getFlats({ section: sectionId, floor: floorId })" :key="flat.number" cols="12">
         <v-card>
-          <v-card-title>Квартира №{{ flat.number }}</v-card-title>
-          <v-card-subtitle>
+          <v-card-title class="blue-grey--text" :class="emptyFlatStyle(flat)">Квартира №{{ flat.number }}</v-card-title>
+          <v-card-subtitle class="blue-grey--text" :class="emptyFlatStyle(flat)">
             <span v-if="flat.rooms != null">Комнат: {{ flat.rooms }}<br /></span>
             <span v-if="flat.square != null">Размер: {{ flat.square }} кв.м.</span>
           </v-card-subtitle>
           <v-card-actions>
             <v-spacer></v-spacer>
-            <v-btn icon @click="chat">
+            <v-btn v-if="!emptyFlat(flat)" icon @click="chat">
               <v-icon>mdi-chat-outline</v-icon>
             </v-btn>
           </v-card-actions>
@@ -59,6 +59,13 @@ export default {
     },
     toastClose() {
       this.toast.show = false;
+    },
+    emptyFlat(flat) {
+      return flat.residents.length == 0;
+    },
+    emptyFlatStyle(flat) {
+      if (this.emptyFlat(flat)) return { "text--lighten-3": true };
+      else return { "text--darken-4": true };
     },
     ...mapMutations(["setTitle"]),
   },
