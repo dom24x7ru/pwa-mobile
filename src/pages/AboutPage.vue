e62c5f54831771616c690465734b9e23792cdf53
Timur Evgazhukov <evgajukov@gmail.com>
2020-11-10 12:21:41 +0300

    страница о приложении

--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -1,5 +1,15 @@
 <template>
-  <v-container fluid></v-container>
+  <v-container fluid>
+    <v-row class="text-center">
+      <v-col cols="12">
+        <v-img :src="require('@/assets/logo.png')" class="my-3" contain height="128" />
+      </v-col>
+      <v-col class="mb-4">
+        <h1 class="display-2 font-weight-bold mb-3">Dom24x7</h1>
+        <p class="subheading font-weight-regular">Версия 1.0.2</p>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
