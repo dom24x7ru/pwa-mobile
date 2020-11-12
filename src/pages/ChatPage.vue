9985ce8b8d276c3e15a20a655a5c2a536cfcba56
Timur Evgazhukov <evgajukov@gmail.com>
2020-11-12 11:44:13 +0300

    обновил страницы чата и сервисов

--- src/pages/ChatPage.vue
+++ src/pages/ChatPage.vue
@@ -1,9 +1,25 @@
 <template>
-  <v-container fluid></v-container>
+  <v-container fluid>
+    <v-row dense>
+      <v-col cols="12">
+        <v-card color="red" dark>
+          <v-card-title>В разработке</v-card-title>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
+import { mapMutations } from "vuex";
+
 export default {
   name: "ChatPage",
+  created() {
+    this.setTitle("Чаты");
+  },
+  methods: {
+    ...mapMutations(["setTitle"]),
+  },
 };
 </script>
--- src/pages/ServicesPage.vue
+++ src/pages/ServicesPage.vue
@@ -1,9 +1,44 @@
 <template>
-  <v-container fluid></v-container>
+  <v-container fluid>
+    <v-row dense>
+      <v-col cols="6">
+        <v-card color="#1F7087" dark>
+          <v-card-title>Инструкции</v-card-title>
+          <v-card-subtitle>Доступно: 0</v-card-subtitle>
+        </v-card>
+      </v-col>
+      <v-col cols="6">
+        <v-card color="#1F7087" dark>
+          <v-card-title>Документы</v-card-title>
+          <v-card-subtitle>Доступно: 0</v-card-subtitle>
+        </v-card>
+      </v-col>
+      <v-col cols="6">
+        <v-card color="#1F7087" dark>
+          <v-card-title>Голосование</v-card-title>
+          <v-card-subtitle>Доступно: 0</v-card-subtitle>
+        </v-card>
+      </v-col>
+      <v-col cols="6">
+        <v-card color="#1F7087" dark>
+          <v-card-title>Контакты</v-card-title>
+          <v-card-subtitle>Доступно: 0</v-card-subtitle>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
+import { mapMutations } from "vuex";
+
 export default {
   name: "ServicesPage",
+  created() {
+    this.setTitle("Сервисы");
+  },
+  methods: {
+    ...mapMutations(["setTitle"]),
+  },
 };
 </script>
