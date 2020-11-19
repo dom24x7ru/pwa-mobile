50e09b7d5a590e62df45354bb7e831116900fd76
Timur Evgazhukov <evgajukov@gmail.com>
2020-11-19 20:35:46 +0300

    исправлена мелкая ошибка с аватаром

--- src/pages/ProfilePage.vue
+++ src/pages/ProfilePage.vue
@@ -3,7 +3,7 @@
     <v-card class="v-card--material">
       <div class="d-flex">
         <v-avatar color="primary" size="128" class="mx-auto v-card--material__avatar">
-          <span class="white--text headline">ЕТХ</span>
+          <span class="white--text headline">{{ avatar }}</span>
         </v-avatar>
       </div>
       <v-card-title>{{ fio }}</v-card-title>
@@ -19,7 +19,7 @@ export default {
   name: "ProfilePage",
   computed: {
     fio() {
-      if (this.user == null) return "";
+      if (this.user == null || this.user.person == null) return "";
       const profile = this.user.person;
       const name = profile.name != null ? profile.name : "";
       const surname = profile.surname != null ? profile.surname : "";
@@ -28,16 +28,16 @@ export default {
     },
     avatar() {
       // пока не поддерживаем картинки, поэтому отображаем первые буквы имени
-      if (this.user == null) return "";
+      if (this.user == null || this.user.person == null) return "";
       const profile = this.user.person;
       let result = "";
-      result += profile.name != null ? profile.name.substr(0, 1) : "";
       result += profile.surname != null ? profile.surname.substr(0, 1) : "";
+      result += profile.name != null ? profile.name.substr(0, 1) : "";
       result += profile.midname != null ? profile.midname.substr(0, 1) : "";
       return result;
     },
     address() {
-      if (this.user == null) return "";
+      if (this.user == null || this.user.person == null) return "";
       const flat = this.user.resident.flat;
       return `подъезд ${flat.section}, этаж ${flat.floor}, кв. ${flat.number}`;
     },
