66d758a50987d617adbb08907472c39081e731a3
Timur Evgazhukov <evgajukov@gmail.com>
2020-11-19 15:10:17 +0300

    поддержка запуска localhost под https,  также пример кода для работы с нотификациями

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,13 @@
+const fs = require("fs");
 module.exports = {
   "transpileDependencies": [
     "vuetify"
-  ]
+  ],
+  "devServer": {
+    "https": {
+      "key": fs.readFileSync("./certs/example.com+5-key.pem"),
+      "cert": fs.readFileSync("./certs/example.com+5.pem"),
+    },
+    "public": "https://localhost:8080/",
+  },
 }
