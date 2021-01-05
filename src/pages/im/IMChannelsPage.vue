1da8453d3b16526124f737b2ca9783955c762cb4
Timur Evgazhukov <evgajukov@gmail.com>
2021-01-05 14:43:28 +0300

    в списке каналов показывать еще и автора последнего сообщения

--- src/pages/im/IMChannelsPage.vue
+++ src/pages/im/IMChannelsPage.vue
@@ -4,7 +4,10 @@
       <v-list-item v-for="channel of imChannels" :key="channel.id" :to="{ name: 'imMessages', params: { channelId: channel.id } }">
         <v-list-item-content>
           <v-list-item-title>{{ channel.title }}</v-list-item-title>
-          <v-list-item-subtitle v-if="channel.lastMessage">{{ channel.lastMessage.body.text }}</v-list-item-subtitle>
+          <v-list-item-subtitle v-if="channel.lastMessage">
+            {{ channel.lastMessage.person | showName }}
+            {{ channel.lastMessage.body.text }}
+          </v-list-item-subtitle>
         </v-list-item-content>
         <v-list-item-action>
           <v-list-item-action-text v-if="channel.lastMessage">{{ channel.lastMessage.createdAt | dtFormat }}</v-list-item-action-text>
@@ -30,6 +33,17 @@ export default {
     ...mapMutations(["setTitle"]),
   },
   filters: {
+    showName(profile) {
+      const empty = value => value == null || value.trim().length == 0;
+
+      if (profile == null) return "";
+      const flat = profile.flat;
+      const name = profile.name != null ? profile.name : "";
+      const surname = profile.surname != null ? profile.surname : "";
+      const midname = profile.midname != null ? profile.midname : "";
+      const result = `${surname} ${name} ${midname}`;
+      return empty(result) ? `Сосед(ка) из кв. №${flat.number}, этаж ${flat.floor}, подъезд ${flat.section}:` : `${result}:`;
+    },
     dtFormat(value) {
       if (value == null) return "";
       return moment(value).fromNow();
